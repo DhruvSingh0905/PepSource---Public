f397521ea3ffddcdc1a555fc36b0f0ae4c13db9a
Surya <SuryaTJalapati@gmail.com>
2025-03-22 10:17:52 -0500

    more env changes

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,7 +1,7 @@
 // supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY; // You can create this in Supabase, instead of using the service key
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY; // You can create this in Supabase, instead of using the service key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
