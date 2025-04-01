efffa41783b4e61e6176ece11916e6a2178decfc
Dhruv Singh <dhruvsingh0905@gmail.com>
2025-03-31 20:54:30 -0500

    ssl fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -8,14 +7,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: process.env.VITE_BACKEND_PRODUCTION_URL || 'http://localhost:8000',
-        secure: false
-      }
-    },
-    https: {
-      key: undefined,
-      cert: undefined
+      '/api': 'http://localhost:8000'
+
     }
   },
   plugins: [react()],
